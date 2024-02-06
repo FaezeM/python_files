4ba224a0a8ec3684ded98efb2ccada89331645d4
FaezeM <f.memari76@gmail.com>
2024-02-06 15:29:53 +0100

    double_check

--- reading_files.py
+++ reading_files.py
@@ -4,25 +4,74 @@ def is_even(num):
     else:
         return "odd"
 
-try:
+def odd_even(src_str):
+    try:
 
-    with open("numbers.txt" , "r") as file:
+        with open(src_str , "r") as file:
 
-        content = file.readlines()
+            content = file.readlines()
+            
+            for line in content:
+
+                try:
+
+                    num = int(line.strip())
+                    stat = is_even(num)
+                    print(f"{num} is {stat}.")
+
+                except ValueError:
+                    "This is not an integer!"
+
+    except FileNotFoundError:
+
+        print("File not found!")
+        return 0
+    
+def double_check(m , n):
+    if m == n:
+        return True
+    else:
+        return False
+
+def check_line_by_line(src_str):
+
+    try:
+        prog = 0
+        with open(src_str , "r" , encoding = "utf-8") as file:
+
+            for line in file:
+
+                prog += line.count("prog")
+
+        print(prog)
+        return
+
+    except FileNotFoundError:
+        print("File not found!")
+        return 0
+
+def check_whole_string(src_str):
+    try:
+        prog = 0
+        with open(src_str , "r" , encoding = "utf-8") as new_file:
+
+            content = new_file.readlines()
+            for line in content:
+
+                prog += line.count("prog")
+
+            print(prog)
+            return
         
-        for line in content:
+    except FileNotFoundError:
+        print("File not found!")
+        return 0
 
-            try:
 
-                num = int(line.strip())
-                stat = is_even(num)
-                print(f"{num} is {stat}.")
-
-            except ValueError:
-                "This is not an integer!"
-
-except FileNotFoundError:
-
-    print("File not found!")
+#odd_even("numbers.txt")
+#check_line_by_line("programming_languages.txt")
+#check_whole_string("programming_languages.txt")
+    
+print(double_check(check_line_by_line("programming_languages.txt") , check_whole_string("programming_languages.txt")))
 
 input("Press any key to exit...")
